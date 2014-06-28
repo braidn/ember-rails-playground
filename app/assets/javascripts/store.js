d09358950f13a9e3b350ed40d796bd08bc8dd198
Braden <braden.douglass@gmail.com>
2014-06-28 19:55:06 -0400

    Lead Class
    Since this is a crm of sorts we are going to need some kind of way to
    house and CRUD leads. First implementation of migration, model,
    controller and serializer.

--- app/assets/javascripts/store.js
+++ app/assets/javascripts/store.js
@@ -1,9 +1,9 @@
 App.Store = DS.Store.extend({
-
+  namespace: 'api/v1'
 });
 
 // Override the default adapter with the `DS.ActiveModelAdapter` which
 // is built to work nicely with the ActiveModel::Serializers gem.
 App.ApplicationAdapter = DS.ActiveModelAdapter.extend({
-
+  // Ember Rails has active_model_serializers as a dependency
 });
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
   get '*path', to: 'home#index'
+
+  namespace :api do
+    namespace :v1 do
+      resources :leads
+    end
+  end
 end
