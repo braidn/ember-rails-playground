54548671083e009e12847611a6b5c17205d0a30d
Braden <braden.douglass@gmail.com>
2014-06-29 12:17:04 -0400

    Ability To Update a Lead
    Give the user the ability to update a lead. Depending on if the
    lead/model is dirty or saving, show a warning message depending on this.

---
+++ app/assets/javascripts/controllers/lead.js
@@ -0,0 +1,12 @@
+App.LeadController = Em.ObjectController.extend({
+  showUnsavedMessage: function() {
+    //used in an if block so needs to return true/false
+    return this.get('isDirty') && !this.get('isSaving')
+  }.property('isDirty', 'isSaving')
+
+  actions: {
+    saveChanges: function() {
+      this.get('model').save()
+    }
+  }
+})
